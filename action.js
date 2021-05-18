b61c95ed141ed8023c74123cff45115d33273160
Akshara Vinod <60498456+aksharavinod@users.noreply.github.com>
2021-05-18 19:16:37 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function pink__btn(){
+    document.body.style.background=" rgb(253, 9, 253)"
+   }
+   
+   function orange__btn()
+   {
+       document.body.style.background=" rgb(250, 59, 11)"
+   }
+   
+   function violet__btn(){
+       document.body.style.background=" rgb(114, 4, 77)"
+   }
